86d7f43cf71cad405e9e71fd9e3ec11002dd3770
backsoul <danielsarmientoenterprise@gmail.com>
2021-11-05 09:33:07 -0500

    update whitelise

--- app/settings.py
+++ app/settings.py
@@ -42,8 +42,8 @@ SECRET_KEY = config('SECRET_KEY')
 DEBUG = False
 
 # ALLOWED_HOSTS = ['back.articles.backsoul.xyz']
-ALLOWED_HOSTS = ['https://articles.backsoul.xyz']
-CORS_ORIGIN_WHITELIST = ('https://articles.backsoul.xyz',)
+ALLOWED_HOSTS = ['https://articles.backsoul.xyz','https://backsoul-articles-back.herokuapp.com']
+CORS_ORIGIN_WHITELIST = ('https://articles.backsoul.xyz','https://backsoul-articles-back.herokuapp.com')
 # Application definition
 
 INSTALLED_APPS = [
--- article/admin.py
+++ article/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Article
 
-admin.site.register(Article)
+class ArticleAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_at', 'list_categories')
+    list_filter = ('created_at', 'title')
+    search_fields = ['title']
+    
+    def list_categories(self, obj):
+        return ', '.join([c.name for c in obj.categories.all()])
+
+admin.site.register(Article,ArticleAdmin)
